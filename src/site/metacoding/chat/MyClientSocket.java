0d5fedfd368beb9560b5807a3b634846e2678943
gih1214 <gih1214@hanmail.net>
2022-02-15 11:37:40 +0900

    Socket 일대일 통신(전이중) 공부

--- src/site/metacoding/chat/MyClientSocket.java
+++ src/site/metacoding/chat/MyClientSocket.java
@@ -1,21 +1,53 @@
 package site.metacoding.chat;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class MyClientSocket {
 
     Socket socket;
     BufferedWriter writer;
+    Scanner sc;
+
+    // 추가 (서버에게 메시지 받기)
+    BufferedReader reader;
 
     public MyClientSocket() {
         try {
-            socket = new Socket("192.168.0.132", 1077);
+            socket = new Socket("localhost", 1077);
             writer = new BufferedWriter(
                     new OutputStreamWriter(socket.getOutputStream()));
-            writer.write("구아현\n"); // buffer에 담기만 했다.
-            writer.flush(); // 물 내리기
+
+            // 메시지 받기
+            reader = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+
+            // 2. 이 부분 추가 (클라이언트 소켓쪽)
+            new Thread(() -> {
+                while (true) {
+                    try {
+                        String inputData = reader.readLine();
+                        System.out.println("받은 메시지 : " + inputData);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+            // 키보드 입력 (반복x)
+            sc = new Scanner(System.in);
+
+            // 키보드로부터 입력 받는 부분 (반복)
+            while (true) {
+                String inputData = sc.nextLine();
+                writer.write(inputData + "\n"); // buffer에 담기만 했다.
+                writer.flush(); // 물 내리기
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/site/metacoding/chat/MyServerSocket.java
+++ src/site/metacoding/chat/MyServerSocket.java
@@ -1,15 +1,22 @@
 package site.metacoding.chat;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class MyServerSocket {
 
     ServerSocket serverSocket; // 리스너 (연결 = 세션)
     Socket socket; // 메시지 통신
     BufferedReader reader;
+
+    // 추가 (클라이언트에게 메시지 보내기)
+    BufferedWriter writer;
+    Scanner sc;
 
     public MyServerSocket() {
         try {
@@ -20,9 +27,32 @@ public class MyServerSocket {
             socket = serverSocket.accept(); // while을 돌면서 대기 (랜덤포트)
             reader = new BufferedReader(
                     new InputStreamReader(socket.getInputStream()));
-            String inputData = reader.readLine();
-            System.out.println("받은 메시지 : " + inputData);
-            System.out.println("클라이언트 연결됨");
+
+            // 메시지 보내기
+            writer = new BufferedWriter(
+                    new OutputStreamWriter(socket.getOutputStream()));
+            sc = new Scanner(System.in);
+
+            // 1. 이 부분 추가 (서버 소켓쪽)
+            new Thread(() -> {
+                while (true) {
+                    try {
+                        String inputData = sc.nextLine(); // 키보드
+                        writer.write(inputData + "\n");
+                        writer.flush();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+            // 메시지 반복해서 받는 서버 소켓 - 메인 스레드
+            while (true) {
+                String inputData = reader.readLine();
+                System.out.println("받은 메시지 : " + inputData);
+                // System.out.println("클라이언트 연결됨");
+            }
+
         } catch (Exception e) {
             System.out.println("통신 오류 발생 : " + e.getMessage());
             // e.printStackTrace();
